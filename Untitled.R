e615c1392c5b801e27fa9388c235c0e35eea1c57
L <y393913402@gmail.com>
2018-09-02 12:38:37 -0400

    correct one

--- Untitled.R
+++ Untitled.R
@@ -132,7 +132,7 @@ X.mean<-apply(X,1,mean)
 X.sd<-apply(X,1,sd)
 y.mean<-mean(y)
 XX<-scale(X)
-yy<-scale(y,scale=FALSE)
+yy<-scale(y)
 
 ## Let's start
 beta_path<-matrix(NA,ndim,0)
@@ -142,6 +142,7 @@ muhat<-rep(0,nobs)
 betahat<-rep(0,ndim)
 # empty active set
 active_set<-rep(FALSE,ndim)
+nonzero_set<-rep(FALSE,ndim)
 # all-zero sign set
 sign_set<-rep(0,ndim)
 
@@ -153,15 +154,15 @@ cc<-t(XX)%*%(yy-muhat)
 sign_set<-sign(cc)
 # active set is initialized using the largest correlation
 active_set[which.max(abs(cc))]<-TRUE
-done<-FALSE
 cnt<-0
-while(!done&cnt<=20){
+
+while(!all(nonzero_set)){
   cnt<-cnt+1
   # correlation vector (help us check equality of correlations)
   cc<-t(XX)%*%(yy-muhat)
   # update sign set
   sign_set<-sign(cc)
-  
+
   # Formula (2.4-2.7) in Efron et al (2004)
   one<-rep(1,sum(active_set))
   X_A<-sweep(as.matrix(XX[,active_set]),2,sign_set[active_set],"*")
@@ -182,6 +183,9 @@ while(!done&cnt<=20){
   vpos[vpos<=0]<-Inf
   vpos[active_set]<-Inf
   gamma_length<-min(pmin(vneg,vpos))
+
+  # An implied step found in R package lars
+  if(all(active_set))gamma_length<-max(abs(cc))/a_A
   
   # vector d defined right before formula (3.3)
   ddd<-rep(Inf,ndim)
@@ -192,41 +196,36 @@ while(!done&cnt<=20){
   # formula (3.5)
   gamma_tilde<-min(gamma_temp)
   if(gamma_tilde<gamma_length){
-    cat(".\n")
     ## by Lasso modification
     # update muhat formula (3.6)
     muhat<-muhat+gamma_tilde*u_A
     # update betahat formula (3.3)
     betahat[active_set]<-betahat[active_set]+gamma_tilde*ddd[active_set] #sign_set[active_set]
     beta_path<-cbind(beta_path,betahat)
+    nonzero_set<-active_set
+    nonzero_set[which.min(gamma_temp)]<-FALSE
     active_set[which.min(gamma_temp)]<-FALSE
   }else{
-    cat("|",active_set,"\n")
-    ## proceed as usual
+    # if there is nothing to add
+    if(all(active_set))break
+    # otherwise proceed as usual
     # update muhat [formula (2.12)] and betahat
     # the formula for betahat is not found or possibly given in (3.3) in Efron et al. (2004)
     muhat<-muhat+gamma_length*u_A
     betahat[active_set]<-betahat[active_set]+sign_set[active_set]*gamma_length*w_A
     beta_path<-cbind(beta_path,betahat)
-    if(all(active_set)){
-      done<-TRUE
-    }else{
-      # activate the next index
-      active_set[which.min(pmin(vneg,vpos))]<-TRUE
-    }
+    nonzero_set<-active_set
+    # activate the next index
+    active_set[which.min(pmin(vneg,vpos))]<-TRUE
   }
 }
 
+dim(beta_path)
+
 beta_path<-cbind(beta_path,c(solve(t(XX)%*%XX,t(XX)%*%yy)))
-matplot(apply(abs(beta_path),2,sum),t(beta_path),type='l')
-
+matplot(apply(abs(beta_path),2,sum),t(beta_path),type='b',pch="*")
+matplot(t(beta_path),type='b',pch="*")
 round(beta_path,4)
-
-betahat[active_set]+gamma_tilde*ddd[active_set]
-
-
-min(asdf[asdf<0])
-
 
 ###########
 ## check ##
@@ -238,13 +237,16 @@ plot(amodel)
 max(abs(t(amodel$beta)[,-1]-beta_path))
 dim(t(amodel$beta))
 dim(beta_path)
-matplot(apply(abs(beta_path),2,max),t(beta_path),type='l')
+matplot(apply(abs(beta_path),2,max),t(beta_path),type='b',pch="*")
 
 
-amodel2<-lars(XX,yy,type="lasso")
+amodel2<-lars(XX,yy,type="lasso",trace=TRUE)
 plot(amodel2,breaks=F)
-round(t(amodel2$beta)[,2:13]-beta_path[,1:12],3)
+matplot(apply(abs(amodel2$beta),1,sum)[1:17],amodel2$beta[1:17,],type='b',pch="*")
+matplot(apply(abs(beta_path),2,sum)[c(1:15,19)],t(beta_path)[c(1:15,19),],type='b',pch="*")
+round(t(amodel2$beta)[,2:16]-beta_path[,1:15],3)
 dim(amodel2$beta)
 dim(beta_path)
 image(round(t(amodel2$beta),4)==0)
 image(round(beta_path,4)==0)
+dim(amodel2$beta)
